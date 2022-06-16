7f839cff27614d28e18162d2aa34d6fb365848ab
bikash bhandari <bikash.bhandari05@gmail.com>
2022-06-16 10:44:21 +0545

    swagger added

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,17 +6,30 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+
 use OpenApi\Annotations as OA;
+
+
 
 class Controller extends BaseController
 {
     /**
  * @OA\Info(
- *   version="1.0.0",
- *   title="My API",
- *   @OA\License(name="MIT"),
- *   @OA\Attachable()
+ *      version="1.0.0",
+ *      title="L5 OpenApi",
+ *      description="L5 Swagger OpenApi description"
+ * )
+ *
+ */
+/**
+ * @OA\Get(
+ *     path="/",
+ *     description="Home page",
+ *     @OA\Response(response="default", description="Welcome page")
  * )
  */
+
+
+
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
